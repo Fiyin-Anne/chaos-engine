4095c97ad7f04fba30d65b085ea2ac9eafbe1835
fiyin <fiyinakinsiku.b@gmail.com>
2021-05-04 08:18:53 +0100

    refactor code for chaining

--- chaos.js
+++ chaos.js
@@ -1,5 +1,11 @@
-var { destroy }    = require('./lib/destroy');
+const Chaos   = require('./lib/chaosengine');
+var { describe }    = require('./lib/describe');
+
+const { destroy, supply  } = Chaos;
 var chaosEngine = {};
 
 chaosEngine.destroy = destroy;
+chaosEngine.supply = supply;
+chaosEngine.describe = describe;
+
 module.exports = chaosEngine;
